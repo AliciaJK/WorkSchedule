5f94787b9e1a076551cc56f09ec318f90883dc87
AliciaJK <alicia.krolak4@gmail.com>
2021-04-14 18:23:27 -0400

    Changed to each fine Added README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # WorkSchedule
+
+This page saves whatever input you have in a particular box and displays it. 
+
+The work scheduler displays any time that has passed as red and any time ahead of the current time as green. 
+
+# Tools used
+Bootstrap @ https://getbootstrap.com/docs/4.0/
+
+Moment @ https://momentjs.com/
---
+++ assets/script.js
@@ -0,0 +1,30 @@
+// need to display time with moment in the header 
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+// need to save data on save button click 
+//need to deal with muliple boxes & saving each one (for loop)
+var words = ''
+
+$('textarea').val('Write your notes here.')
+window.localStorage.setItem("textarea", words);
+    console.log(value);
+    return this.value.trim() //this is the save
+
+
+// save on button click 
+document.querySelector('textarea').addEventListener('Save', funciton(saveButton))
+    var wordsInBoxes
+
+function saveButton()
+//look into if save button is clicked ...then overwrite words variable
+//get item
+//for each 
+// this (important) 
+
+//on save display tasks 
+
+
+
+//need to comapre time of day and time on schedule & change box colour as per css
